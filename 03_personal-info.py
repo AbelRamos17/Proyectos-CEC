04f087da0eb87da77ef11dd7be89f828d13e5036
AbelRamos17 <101692152+AbelRamos17@users.noreply.github.com>
2022-03-20 17:07:42 -0500

    Add files via upload

---
+++ 03_personal-info.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 20 16:44:43 2022
+
+@author: AbelRamos
+"""
+
+Nombre=input("Escriba su nombre:")
+Apellido=input("Escriba su apellido:")
+Edad=input("Escriba su edad:")
+Ciudad=input("Ingrese su ciudad:")
+
+print("¡Hola!", Nombre, Apellido, "hoy cumples", Edad,"años, felicidades", "tu pedido esta listo y entregado en la ciudad de",Ciudad, )

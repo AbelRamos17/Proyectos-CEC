f0a7f6995a02e7b8e883e7ffed3ae2240e78e7a4
AbelRamos17 <101692152+AbelRamos17@users.noreply.github.com>
2022-03-21 02:05:47 -0500

    Add files via upload

---
+++ def4.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 21 01:53:40 2022
+
+@author: AbelRamos
+"""
+
+def resta(a,b):
+    print(a-b)
+    
+resta(5,3)
+resta(3,5)
+resta(a=5,b=3)
+resta(b=3,a=5)
+resta(5,b=3)
---
+++ def5.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 21 01:57:19 2022
+
+@author: AbelRamos
+"""
+
+def mul(a,b):
+    print(a*b)
+    return a*b
+mul(5, 5)
+
+resultado=mul(15, 65)
+opt=resultado+5

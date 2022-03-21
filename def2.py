d518d4d124d769ef579cdbebc3a268a56062348c
AbelRamos17 <101692152+AbelRamos17@users.noreply.github.com>
2022-03-21 01:45:20 -0500

    Add files via upload

---
+++ def2.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 21 01:31:40 2022
+
+@author: AbelRamos
+"""
+
+def fun2 (nombre):
+    print("Hola!",nombre)
+    
+fun2("Ana")
+fun2("Juan")
+fun2("Carlos")
+fun2(4)

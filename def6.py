9419e72fc903a36d3538315eb783b3bcafd86745
AbelRamos17 <101692152+AbelRamos17@users.noreply.github.com>
2022-03-21 02:23:40 -0500

    Add files via upload

---
+++ def6.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 21 02:07:28 2022
+
+@author: AbelRamos
+"""
+
+def funlist(lista):
+     for item in lista:
+         print("Hola",item)
+     print(" ")
+funlist(["Ana"])
+funlist(["Ana","Israel"])
+funlist(["Ana","Israel","Carlos"])
---
+++ def7.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 21 02:12:24 2022
+
+@author: AbelRamos
+"""
+
+def crealista(n):
+    lista=[]
+    for item in range(n):
+        lista.append(item)
+    return lista
+
+print(crealista(10))

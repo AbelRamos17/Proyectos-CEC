2b09cf54f5563e407876a50e1e534ba91483dcde
AbelRamos17 <101692152+AbelRamos17@users.noreply.github.com>
2022-03-20 19:32:59 -0500

    Add files via upload

---
+++ if1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 20 19:13:37 2022
+
+@author: AbelRamos
+"""
+
+datos=100
+nativa=1
+if datos==nativa:
+    print("Los VLAN son iguales")
+else:
+    print("Las VLAN son diferentes")

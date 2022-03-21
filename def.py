e39887a74e3f7058110a343bf6cfd2e963fdfecc
AbelRamos17 <101692152+AbelRamos17@users.noreply.github.com>
2022-03-21 01:17:23 -0500

    Add files via upload

---
+++ def.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 21 00:05:10 2022
+
+@author: AbelRamos
+"""
+
+def mensaje():
+    print("Ingrese un dato")
+    
+mensaje()
+a=input()
+mensaje()
+b=input()
+mensaje()
+c=input()
+mensaje()
+d=input()
+print(a+b+c+d)
